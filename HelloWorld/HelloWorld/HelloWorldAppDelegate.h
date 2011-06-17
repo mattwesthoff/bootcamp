2ff958f154d11029477f73083a1c2ffad4424c2b
Matt Westhoff <mattwesthoff@gmail.com>
2011-06-17 10:46:37 -0500

    adding helloworld project with alert

---
+++ HelloWorld/HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Matt Westhoff on 6/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HelloWorldViewController;
+
+@interface HelloWorldAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet HelloWorldViewController *viewController;
+
+@end
---
+++ HelloWorld/HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Matt Westhoff on 6/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+    
+}
+
+@end
