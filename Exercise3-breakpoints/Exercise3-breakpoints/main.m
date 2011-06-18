94c9741d92bac15ebd66ab8000894e9ced4ec85e
Matt Westhoff <mattwesthoff@gmail.com>
2011-06-18 16:13:07 -0500

    deleting some files

---
+++ Exercise3-breakpoints/Exercise3-breakpoints/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Exercise3-breakpoints
+//
+//  Created by Matt Westhoff on 6/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
---
+++ e8Tabs/Class.h
@@ -0,0 +1,16 @@
+//
+//  Class.h
+//  e8Tabs
+//
+//  Created by Matt Westhoff on 6/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Class : BeesViewController {
+    
+}
+
+@end
